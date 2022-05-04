d1faa9d21179ff325406ce680b8a9a6749d88732
M Taufiq Permana S <35789663+taufiqpsumarna@users.noreply.github.com>
2022-05-04 22:09:34 +0700

    Update todos-list.component.js
    Fix load from env

--- frontend/src/components/todos-list.component.js
+++ frontend/src/components/todos-list.component.js
@@ -21,7 +21,7 @@ export default class TodosList extends Component {
     }
 
     componentDidMount() {
-        axios.get('http://localhost:4000/todos/')
+        axios.get('http://${process.env.API_SERVER}/todos/')
             .then(response => {
                 this.setState({ todos: response.data });
             })
